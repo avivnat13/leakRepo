0b3aded0e8b32b7eff3736ca65b6cd6ba2b3fe0d
avivnat13 <aviv@jit.io>
2025-02-10 18:18:24 +0200

    Update file.py

--- file.py
+++ file.py
@@ -5,6 +5,6 @@ import json
 def exploitable_yaml_load(**kwargs):
     yaml.unsafe_load("!!python/object/new:os.system [echo EXPLOIT!]", **kwargs)
     print("hello world!")
-    print("hello world!")
+    # print("hello world!")
   
  
