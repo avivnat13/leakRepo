07cc45d9fb4cd16be6e440248292fa3c1baeee85
avivnat13 <aviv@jit.io>
2025-02-11 09:14:46 +0200

    Update another_vuln.py

--- another_vuln.py
+++ another_vuln.py
@@ -3,7 +3,7 @@ import base64
 import json
 
 # Hardcoded secrets (Vulnerability)
-API_KEY = "sk_test_1234567890abcdef"  # Mock Stripe API Key
+# API_KEY = "sk_test_1234567890abcdef"  # Mock Stripe API Key
 API_KEY1 = "sk_test_1234567890abcdef"  # Mock Stripe API Key
 DB_PASSWORD = "SuperSecret123!"  # Mock Database Password
 SECRET_KEY = "MyS3cr3tK3y"  # Mock Flask Secret Key
